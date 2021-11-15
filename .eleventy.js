fc37faa77b2f3fa8a0b0fbb0f33f8444b1145426
Matt DeCamp <matthewdecamp@gmail.com>
2021-11-14 20:57:58 -0500

    book feed fixes

--- .eleventy.js
+++ .eleventy.js
@@ -31,24 +31,6 @@ module.exports = function (eleventyConfig) {
     }).toFormat("y-MM-dd");
   });
 
-  // CSV to JSON book feed
-  csvToJson()
-    .fromFile("./_data/books.csv")
-    .then(function(books) {
-      fs.writeFile(
-        "./_data/books.json",
-        JSON.stringify(books, null, 4),
-        function(err) {
-          if (err) {
-            throw err;
-          }
-          console.log("new books feed created!");
-        }
-      );
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
 
   // PLUGINS //
   // RSS
---
+++ js/csvToJson.js
@@ -0,0 +1,18 @@
+// CSV to JSON book feed
+csvToJson()
+  .fromFile("./_data/booklist.csv")
+  .then(function (books) {
+    fs.writeFile(
+      "./_data/books.json",
+      JSON.stringify(books, null, 4),
+      function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log("new books feed created!");
+      }
+    );
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
