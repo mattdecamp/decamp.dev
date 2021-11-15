cead697378272b55c0c2beb353c4cbd8c80799ae
Matt DeCamp <matthewdecamp@gmail.com>
2021-11-14 20:04:58 -0500

    csv to json script

--- .eleventy.js
+++ .eleventy.js
@@ -1,42 +1,59 @@
-
-const { DateTime }  = require('luxon');
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const Image = require("@11ty/eleventy-img");
+// csv to json
+const csvToJson = require("csvtojson");
+const fs = require("fs");
 
-
-module.exports = function(eleventyConfig) {
-
+// eleventy config
+module.exports = function (eleventyConfig) {
   // Passthrough copy
-  eleventyConfig.addPassthroughCopy('fonts');
-  eleventyConfig.addPassthroughCopy('images');
-  eleventyConfig.addPassthroughCopy('js');
-  eleventyConfig.addPassthroughCopy('css');
-  
-  // Layout aliases for convenience 
-  eleventyConfig.addLayoutAlias('default', 'layouts/base.njk');
+  eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("css");
+
+  // Layout aliases for convenience
+  eleventyConfig.addLayoutAlias("default", "layouts/base.njk");
 
   // Grab excerpts and sections from a file
-  eleventyConfig.addFilter("section", require("./js/section.js") );
-  
+  eleventyConfig.addFilter("section", require("./js/section.js"));
+
   // Post Dates
-  eleventyConfig.addFilter('readableDate', dateObj => {
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat('LLLL d, y');
+      zone: "utc",
+    }).toFormat("LLLL d, y");
   });
-  eleventyConfig.addFilter('htmlDate', dateObj => {
+  eleventyConfig.addFilter("htmlDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat('y-MM-dd');
+      zone: "utc",
+    }).toFormat("y-MM-dd");
   });
-  
+
+  // CSV to JSON book feed
+  csvToJson()
+    .fromFile("./_data/books.csv")
+    .then((books) => {
+      fs.writeFile(
+        "./_data/books.json",
+        JSON.stringify(books, null, 4),
+        (err) => {
+          if (err) {
+            throw err;
+          }
+          console.log("new books feed created!");
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   // PLUGINS //
   // RSS
   eleventyConfig.addPlugin(pluginRss);
   // Syntax Highlighting
   eleventyConfig.addPlugin(syntaxHighlight);
-
-
-  
 };
-
