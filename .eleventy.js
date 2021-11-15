f325eab917a0d244a5bd91410f68aa1f65abd521
Matt DeCamp <matthewdecamp@gmail.com>
2021-11-14 20:49:24 -0500

    removed eleventy image variable

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
-const Image = require("@11ty/eleventy-img");
 // csv to json
 const csvToJson = require("csvtojson");
 const fs = require("fs");
