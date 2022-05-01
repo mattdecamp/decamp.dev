f1a505f4252bf9c191c7500d7d25ccb0a61ad4ca
Matt DeCamp <matthewdecamp@gmail.com>
2022-05-01 17:24:40 -0400

    Update notionBooks.js

--- _src/_data/notionBooks.js
+++ _src/_data/notionBooks.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
-const Cache = require("@11ty/eleventy-cache-assets");
+const EleventyFetch = require("@11ty/eleventy-fetch");
 const NOTION_API_KEY = process.env.NOTION_API_KEY;
 const NOTION_BOOKS_DB = process.env.NOTION_BOOKS_DB;
-const url = `https://api.notion.com/v1/databases/${NOTION_BOOKS_DB}/query`;
+
 module.exports = async function () {
-  const filter = {
+  const url = `https://api.notion.com/v1/databases/${NOTION_BOOKS_DB}/query`;
+  const sort = {
     "sorts": [
       {
         "property": "dateRead",
@@ -12,8 +13,9 @@ module.exports = async function () {
       }
     ],
   };
-  let json = await Cache(url, {
-    duration: "5s",
+  let json = await EleventyFetch(url, {
+    directory: ".cache",
+    duration: "1d",
     type: "json",
     fetchOptions: {
       method: "POST",
@@ -22,12 +24,10 @@ module.exports = async function () {
         "Notion-Version": "2022-02-22",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(filter)
+      body: JSON.stringify(sort)
     },
   });
   return {
     json
   }
 }
-
-
