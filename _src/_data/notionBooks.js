e51c4f57ffe0fc2d097d305b991685f9bd4cf34c
Matt DeCamp <matthewdecamp@gmail.com>
2022-04-30 21:58:44 -0400

    scripts for pulling book data from Notion

---
+++ _src/_data/notionBooks.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const Cache = require("@11ty/eleventy-cache-assets");
+const { Client } = require("@notionhq/client");
+const { NOTION_API_KEY, NOTION_BOOKS_DB } = process.env;
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+module.exports = async function () {
+  let url = `https://api.notion.com/v1/databases/${NOTION_BOOKS_DB}/query`;
+  const filter = {
+    "sorts": [
+      {
+        "property": "dateRead",
+        "direction": "descending",
+      }
+    ],
+  };
+  let json = await Cache(url, {
+    duration: "5s",
+    type: "json",
+    fetchOptions: {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${NOTION_API_KEY}`,
+        "Notion-Version": "2022-02-22",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(filter)
+    },
+  });
+  return {
+    json
+  }
+}
+
+
